8cae70677d12559acfc6080ae5dfca265a27e79e
Owoeye Femi <famofash@gmail.com>
2020-07-03 16:11:34 -0400

    Add project files.

---
+++ IManager-CE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IManager_CE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
